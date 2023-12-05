af5f1313268e3fb52b82d56a61a6b2d2d2404d95
Tom Burns <Tomulvlup@gmail.com>
2023-12-05 12:28:53 +0000

    text input basic build

--- Callback/App.js
+++ Callback/App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import ButtonComp from './components/Button';
+import TextComp from './components/TextInput';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     
       <StatusBar style="auto" />
+      <TextComp
+        style={styles.input}
+        placeholder="Enter your name"
+        onChangeText={(text) => console.log(text)}
+      />
       <ButtonComp/>
       
     </View>
--- Callback/components/Button.js
+++ Callback/components/Button.js
@@ -1,15 +1,25 @@
-import { Button } from "react-native";
+import { Button , View, StyleSheet } from "react-native";
 
 export default function ButtonComp() {
     return(
+      <View>
          <Button
         onPress={() => {
           alert('You tapped the button!');
         }}
         title="Learn More"
-        color="#841584"
+        color={'#841584'}
         // accessibilityLabel="Learn more about this purple button"
       /> 
+      </View>
     )
 }
 
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#841584',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
